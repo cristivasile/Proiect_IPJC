1a3df1d4a9be435ad77b4dcd7b19add07569e562
Vasile George-Cristian <79210106+cristivasile@users.noreply.github.com>
2022-10-30 18:53:32 +0200

    Improved spawn offsets

--- Top-Down Shooter/Assets/Scripts/EnemySpawner.cs
+++ Top-Down Shooter/Assets/Scripts/EnemySpawner.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     public List<GameObject> enemies;
-
     public GameObject player;
 
-    public float maxSpawnOffset = 8;
-    public float minSpawnOffset = 5.5f;
+    //TODO - check if enemy is within bounds
+
+    /// xOffset, yOffset
+    public (float, float) minSpawnOffset = (9, 5.5f);
+    public (float, float) maxSpawnOffset = (12, 8);
+
     public int maxEnemyCount = 10;
     /// <summary>
     /// Spawn interval in milliseconds
@@ -38,11 +41,21 @@ public class EnemySpawner : MonoBehaviour
 
     private void Spawn()
     {
-        //var playerPosition = GameMode.playerController.gameObject.transform.position;
-        var xOffset = Random.Range(minSpawnOffset, maxSpawnOffset);
-        var yOffset = Random.Range(minSpawnOffset, maxSpawnOffset);
-
         System.Random random = new();
+        var spawnOnVerticalAxis = random.Next(0, 2);
+        float xOffset, yOffset;
+        if (spawnOnVerticalAxis == 1)
+        {
+            //var playerPosition = GameMode.playerController.gameObject.transform.position;
+            xOffset = Random.Range(minSpawnOffset.Item1, maxSpawnOffset.Item1);
+            yOffset = Random.Range(0, maxSpawnOffset.Item2);
+        }
+        else
+        {
+            xOffset = Random.Range(0, maxSpawnOffset.Item1);
+            yOffset = Random.Range(minSpawnOffset.Item2, maxSpawnOffset.Item2);
+        }
+
         var isXNegative = random.Next(0, 2);
         var isYNegative = random.Next(0, 2);
 
