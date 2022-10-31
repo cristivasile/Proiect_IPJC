471b941cf48dff0ff98c89364a39e14141dd053d
Vasile George-Cristian <79210106+cristivasile@users.noreply.github.com>
2022-10-31 20:11:14 +0200

    Implemented enemy OnHit function
    Added knockback, punch through and damage efects to the bullets which have an effect on the hit enemy

--- Top-Down Shooter/Assets/Scripts/WeaponScripts/BulletController.cs
+++ Top-Down Shooter/Assets/Scripts/WeaponScripts/BulletController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
+    public float damage = 1f;
+    public float knockbackStrength = 5f;
+    /// <summary>
+    /// Number of enemies the bullet can punch through before it is destroyed
+    /// </summary>
+    public int punchThrough = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +26,15 @@ public class BulletController : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Wall"))
             Destroy(gameObject);
+
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            punchThrough--;
+            if (punchThrough == 0)
+                Destroy(gameObject);
+
+            other.gameObject.GetComponent<BaseEnemy>().TakeHit(gameObject, knockbackStrength, damage);
+        }
+
     }
 }
