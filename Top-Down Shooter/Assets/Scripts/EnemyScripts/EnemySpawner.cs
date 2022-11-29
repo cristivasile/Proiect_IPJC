5fe275fae283c46d7da7b754c45f5fc6db44dc5a
Vasile George-Cristian <79210106+cristivasile@users.noreply.github.com>
2022-11-29 14:36:44 +0200

    Improved spawners

--- Top-Down Shooter/Assets/Scripts/EnemyScripts/EnemySpawner.cs
+++ Top-Down Shooter/Assets/Scripts/EnemyScripts/EnemySpawner.cs
@@ -1,16 +1,20 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class EnemySpawner : MonoBehaviour
 {
     public List<GameObject> enemies;
     public GameObject player;
 
-    //TODO - check if enemy is within bounds
+    public GameObject leftBound;
+    public GameObject rightBound;
+    public GameObject topBound;
+    public GameObject bottomBound;
 
     /// xOffset, yOffset
-    public (float, float) minSpawnOffset = (9, 5.5f);
-    public (float, float) maxSpawnOffset = (12, 8);
+    public static (float, float) minSpawnOffset = (9, 5.5f);
+    public static (float, float) maxSpawnOffset = (12, 8);
 
     public int maxEnemyCount = 10;
     /// <summary>
@@ -44,6 +48,29 @@ public class EnemySpawner : MonoBehaviour
     private void Spawn()
     {
         System.Random random = new();
+        var enemyIndex = random.Next(0, enemies.Count);
+        GameObject newEnemy = Instantiate(enemies[enemyIndex]);
+
+        Vector3 enemyPosition;
+        bool positionIsValid = true;
+        //set position
+        do
+        {
+            enemyPosition = getRandomPosition(player.transform.position);
+            //check if the position is valid
+            if (leftBound != null && rightBound != null && topBound != null && bottomBound != null) 
+                //only check if the bounds are defined to prevent an infinite loop
+                positionIsValid = (enemyPosition.x > leftBound.transform.position.x
+                && enemyPosition.x < rightBound.transform.position.x
+                && enemyPosition.y > bottomBound.transform.position.y
+                && enemyPosition.y < topBound.transform.position.y);
+        } while (!positionIsValid);
+        newEnemy.transform.position = enemyPosition;
+    }
+
+    private static Vector3 getRandomPosition(Vector3 playerPosition)
+    {
+        System.Random random = new();
         var spawnOnVerticalAxis = random.Next(0, 2);
         float xOffset, yOffset;
         if (spawnOnVerticalAxis == 1)
@@ -61,12 +88,9 @@ public class EnemySpawner : MonoBehaviour
         var isXNegative = random.Next(0, 2);
         var isYNegative = random.Next(0, 2);
 
-        xOffset = (isXNegative == 0)? xOffset : -xOffset;
+        xOffset = (isXNegative == 0) ? xOffset : -xOffset;
         yOffset = (isYNegative == 0) ? yOffset : -yOffset;
 
-        var enemyIndex = random.Next(0, enemies.Count);
-        GameObject newEnemy = Instantiate(enemies[enemyIndex]);
-        //set position
-        newEnemy.transform.position = player.transform.position + new Vector3(xOffset, yOffset, 0);
+        return playerPosition + new Vector3(xOffset, yOffset);
     }
 }
